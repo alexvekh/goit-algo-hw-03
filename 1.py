d77100d6fe7c38b063c983fb680eaf42c5d3e569
Oleksiy Vekhulevskyy <alexvekh@yahoo.com>
2024-05-02 01:10:12 -0400

    added printout

--- 1.py
+++ 1.py
@@ -3,23 +3,19 @@ import argparse
 import shutil
 
 def copy_files(source_dir, dest_dir=None):
-  """
-  Функція рекурсивно копіює файли з вихідної директорії до нової директорії, сортуючи їх у піддиректорії за розширенням.
+  """ Функція рекурсивно копіює файли з вихідної директорії до нової директорії, сортуючи їх у піддиректорії за розширенням.
+      Args:  source_dir: Шлях до вихідної директорії.
+             dest_dir: Шлях до директорії призначення (за замовчуванням ./dist).  """
+  
+  if not os.path.exists(source_dir):
+    print(f'Sorry. Directory {source_dir} does not exist.')
 
-  Args:
-      source_dir: Шлях до вихідної директорії.
-      dest_dir: Шлях до директорії призначення (за замовчуванням ./dist).
-  """
-
-# python 3.py ./s ./d
-
-
-  print("func started")
   if not dest_dir:
     dest_dir = os.path.join(os.path.dirname(source_dir), 'dist')
 
   os.makedirs(dest_dir, exist_ok=True)
 
+  # The nested loop structure ensures that the os.walk function is called recursively for each subdirectory encountered
   for root, _, files in os.walk(source_dir):
     for file in files:
       # Отримання розширення файлу
@@ -34,6 +30,7 @@ def copy_files(source_dir, dest_dir=None):
       dest_path = os.path.join(sub_dir, file)
       try:
         shutil.copy(source_path, dest_path)
+        print(f"File {file} copied to {sub_dir} successfully")
       except OSError as e:
         print(f'Error copying file {source_path}: {e}')
 
@@ -43,10 +40,7 @@ if __name__ == '__main__':
   # Додавання аргументів
   parser.add_argument('source_dir', help='Path to the source directory.')
   parser.add_argument('dest_dir', nargs='?', help='Path to the destination directory. Default: ./dist', default='./dist')
-  print(parser)
   # Обробка аргументів
   args = parser.parse_args()
-  print(parser)
-  print("Parsed:", args)
   # Виклик функції копіювання файлів
   copy_files(args.source_dir, args.dest_dir)
