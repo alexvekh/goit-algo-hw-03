80d379ccf03f61088a398bc8516fd3db9a73d99a
Oleksiy Vekhulevskyy <alexvekh@yahoo.com>
2024-05-03 01:22:56 -0400

    fixed mentor recomendation

--- 1.py
+++ 1.py
@@ -1,46 +1,56 @@
+# python 1.py source_dir dest_dir
+
+from pathlib import Path
 import os
 import argparse
 import shutil
 
-def copy_files(source_dir, dest_dir=None):
-  """ Функція рекурсивно копіює файли з вихідної директорії до нової директорії, сортуючи їх у піддиректорії за розширенням.
-      Args:  source_dir: Шлях до вихідної директорії.
-             dest_dir: Шлях до директорії призначення (за замовчуванням ./dist).  """
-  
-  if not os.path.exists(source_dir):
-    print(f'Sorry. Directory {source_dir} does not exist.')
+def copy_files_by_ext(source_path: Path, dest_dir: Path) -> None:
+  for child in source_path.iterdir():
+    if child.is_dir():
+      copy_files_by_ext(child, dest_dir)    ###   <---  Рекурсія   
+    else:
+      ext = child.suffix[1:]
 
-  if not dest_dir:
-    dest_dir = os.path.join(os.path.dirname(source_dir), 'dist')
+      sub_dir = dest_dir/ext.lower()
 
-  os.makedirs(dest_dir, exist_ok=True)
-
-  # The nested loop structure ensures that the os.walk function is called recursively for each subdirectory encountered
-  for root, _, files in os.walk(source_dir):
-    for file in files:
-      # Отримання розширення файлу
-      filename, extension = os.path.splitext(file)
-
-      # Створення шляху до піддиректорії
-      sub_dir = os.path.join(dest_dir, extension[1:])
-      os.makedirs(sub_dir, exist_ok=True)
+      if not os.path.exists(sub_dir):
+        os.makedirs(sub_dir, exist_ok=True)
 
       # Копіювання файлу
-      source_path = os.path.join(root, file)
-      dest_path = os.path.join(sub_dir, file)
-      try:
-        shutil.copy(source_path, dest_path)
-        print(f"File {file} copied to {sub_dir} successfully")
-      except OSError as e:
-        print(f'Error copying file {source_path}: {e}')
+      source_file = source_path/child.name
+      dest_file = sub_dir / child.name
+
+      if not os.path.exists(dest_file):
+        try:
+          shutil.copy(source_file, dest_file)
+          print(f"File '{child.name}' copied to '{sub_dir}' successfully.")
+        except OSError as e:
+          print(f'Error copying file {child}: {e}')
+      else:
+        print(f"File '{child.name}' already exists in '{sub_dir}'. Skipping copy.")
+
 
 if __name__ == '__main__':
-  # Створення об'єкта аргументів командного рядка
+  # Парсинг аргументів командного рядка
   parser = argparse.ArgumentParser(description='Script to copy files.')
-  # Додавання аргументів
   parser.add_argument('source_dir', help='Path to the source directory.')
   parser.add_argument('dest_dir', nargs='?', help='Path to the destination directory. Default: ./dist', default='./dist')
-  # Обробка аргументів
   args = parser.parse_args()
-  # Виклик функції копіювання файлів
-  copy_files(args.source_dir, args.dest_dir)
+
+  source_dir = Path(args.source_dir)
+  dest_dir = Path(args.dest_dir)
+
+  # Check for exist source dir
+  if not os.path.exists(source_dir):
+    print(f'Sorry. Directory {source_dir} does not exist.')
+  
+  # Create dest dir(if need)
+  try:
+    os.makedirs(dest_dir, exist_ok=True)
+    print(f"Directory {dest_dir} is ready.")
+  except OSError as e:
+    print(f"Error creating directory: {e}")
+  
+  # Call copy func
+  copy_files_by_ext(source_dir, dest_dir)
